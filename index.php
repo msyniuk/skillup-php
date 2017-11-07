c59b78624e31f897c9b0e9f204d5fc024a8d70f8
Mikhail Syniuk <msyniuk@gmail.com>
2017-11-07 21:23:06 +0200

    Homework10 Task 1-8

--- index.php
+++ index.php
@@ -1,40 +1,63 @@
 <?php
-    $arr = ['html', 'css', 'php', 'js', 'jq'];
+    define('BR', '<br/>');
+    $a = 10;
+    $b = 2;
+    echo 'Остаток от деления a на b = ' . $a % $b . BR;
 
-	foreach ($arr as $value){
-        echo $value . PHP_EOL;
+    if ($a % $b == 0){
+        echo 'Делится. Результат деления = ' . $a / $b . BR;
+    } else {
+        echo 'Не делится. Остаток от деления = ' . $a % $b . BR;
     }
+
+
+    $str = 'html css php';
+    $arr = explode(' ', $str);
+    print_r ($arr);
+    print BR;
 
     $arr = [1, 2, 3, 4, 5];
+    $result = array_slice($arr,1, 3);
+    print_r($result);
+    print BR;
+    array_splice($arr,1,2);
+    print_r($arr);
+    print BR;
+    $arr = [1, 2, 3, 4, 5];
+    array_splice($arr,1,2, ['a', 'b', 'c']);
+    print_r($arr);
+    print BR;
 
-    $result = 0;
-    foreach ($arr as $value){
-        $result += $value;
-    }
-    echo PHP_EOL;
-    echo $result . PHP_EOL;
+    $arr = [1, 2, 3, 4, 5];
+    $average = array_sum($arr)/count($arr);
+    echo $average . BR;
 
-    $result = 0;
-    foreach ($arr as $value){
-        $result += $value * $value;
-    }
-    echo PHP_EOL;
-    echo $result . PHP_EOL;
+    echo 'Сумма чисел от 1 до 100 = ' . array_sum(range(1,100)) . BR;
 
-    $arr = ['green'=>'зеленый', 'red'=>'красный','blue'=>'голубой'];
-    foreach ($arr as $key => $value){
-        echo $key . ' - ' . $value . PHP_EOL;
-    }
-    echo PHP_EOL;
-    $arr = ['Коля'=>'200', 'Вася'=>'300','Петя'=>'400'];
-    foreach ($arr as $key => $value){
-        echo $key . ' - зарплата ' . $value . ' долларов' . PHP_EOL;
-    }
-    echo PHP_EOL;
-    for ($i = 1; $i<=100; $i++){
-        echo $i . PHP_EOL;
-    }
-    echo PHP_EOL;
-    for ($i = 11; $i<=33; $i++){
-        echo $i . PHP_EOL;
-    }
+    $arr = range(1,100);
+    $str = implode('<br/>', $arr);
+    echo $str . BR;
+
+    $arr = array_fill(0,10, 'x');
+    print_r($arr);
+    print BR;
+
+    $n = 5;
+    $arr = range(1, $n);
+    $factorial = array_product($arr);
+    echo 'Факториал числа ' . $n . ' = ' . $factorial . BR;
+
+    $num = 12345;
+    $arr = str_split((string) $num);
+    $sum = array_sum($arr);
+    echo 'Сумма цифр числа ' . $num . ' = ' . $sum . BR;
+
+    $str = 'hello, sea';
+    $str = substr_replace($str, ucfirst($str[strlen($str)-1]), strlen($str)-1);
+    echo $str . BR;
+
+
+
+
+
+
