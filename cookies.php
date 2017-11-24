6e1ee0c5fb31d79fe32f727f9721c0310597681c
Mikhail Syniuk <msyniuk@gmail.com>
2017-11-24 14:20:16 +0300

    cookies task finish

--- cookies.php
+++ cookies.php
@@ -1,4 +1,16 @@
 <?php
+define('BR', '<br/>');
+print '<h3>Счетчик на куках (задача 6)</h3>';
+if(!isset($_COOKIE['count'])){
+    print 'Вы еще не обновляли эту страницу.' . BR;
+    setcookie('count', 1, time() + 86400);
+} else {
+    $count = $_COOKIE['count'] + 1;
+    setcookie('count', $count, time() + 86400);
+    print 'Вы обновили эту страницу ' . $_COOKIE['count'] . ' раз.' . BR;
+}
+
+//Задача 3
 setcookie('age', mt_rand(10, 70)); // до закрытия браузера
 
 setcookie('age', mt_rand(10, 70), time() + 3600); // на час
@@ -17,4 +29,25 @@ setcookie('age', mt_rand(10, 70), time() + $expire); // до конца теку
 $expire1 = 3600 * 24 * 365 - (date('n')-1) * 3600 * 24 *30 - (date('j')-1) * 3600 * 24 - $expire;
 setcookie('age', mt_rand(10, 70), time() + $expire1); // до конца текущего года
 
-echo $_COOKIE['age'];
+print '<h3>Задача 3</h3>';
+echo 'Возраст из установленной куки age - ' . $_COOKIE['age'] . BR;
+
+print '<h3>Баннер (задача 7)</h3>';
+
+if(!isset($_COOKIE['banner_hide'])){
+    $hide ='visibility: visible;';
+    setcookie('banner_hide', 'hide', time() + 86400 * 30);
+} else {
+    $hide ='visibility: hidden;';
+}
+?>
+<div>
+    <div style="background-image:url('banner.jpg'); width: 200px; height: 200px; <?= $hide ?>">
+        <div style="position: relative; top: 160px; left: 30px">
+            <form action="" method="POST">
+                <input name="hide" type="submit" value="Не показывать"/>
+            </form>
+        </div>
+    </div>
+</div>
+
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 define('BR', '<br/>');
 session_start();
-
+print '<h3>Счетчик на сессии (задача 4)</h3>';
 if(!isset($_SESSION['count'])){
     print 'Вы еще не обновляли эту страницу.';
     $_SESSION['count'] = 0;
@@ -14,6 +14,7 @@ if(!isset($_SESSION['count'])){
 ?>
 <p>
     <a href="regexp.php">Регулярные выражения (задачи)</a><br/>
-    <a href="session.php">Сессии (задача 3)</a>
+    <a href="session.php">Сессии (задача 3)</a><br/>
+    <a href="cookies.php">Cookies (задачи 3, 6, 7)</a>
 </p>
 
